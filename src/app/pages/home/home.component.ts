2b0007dfdd114925570788870e171a7feadb7b37
Victor Eduardo <VictorOdarp@gmail.com>
2024-09-09 22:10:15 -0300

    Desenvolvendo a primeira ligação com o banco de dados para pegar a lista de funcionarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { Funcionario } from '../../models/Funcionarios';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  funcionarios: Funcionario[] = [];
+  funcionariosGeral : Funcionario[] = [];
 
+  constructor(private funcionarioService: FuncionarioService) {}
+
+  ngOnInit(): void {
+    this.funcionarioService.GetFuncionarios().subscribe(data => {
+
+    }); 
+  }
 }
---
+++ src/app/services/funcionario.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Response } from '../models/Response';
+import { Funcionario } from '../models/Funcionarios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+  private apiUrl =  `${environment.ApiUrl}/Funcionario`
+
+  constructor(private http: HttpClient) { }
+
+  GetFuncionarios() : Observable<Response<Funcionario[]>> {
+    return this.http.get<Response<Funcionario[]>>(this.apiUrl); 
+  }
+}
+ 
