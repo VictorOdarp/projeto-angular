ff2af9cb3696bb9adfe7befebce622a4c203eab1
Victor Eduardo <VictorOdarp@gmail.com>
2024-09-19 23:13:27 -0300

    Capturando as informações digitadas nos inputs da página cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { CommonModule } from '@angular/common';
-import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 @Component({
   selector: 'app-root',
--- src/app/componentes/funcionario-form/funcionario-form.component.ts
+++ src/app/componentes/funcionario-form/funcionario-form.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-funcionario-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './funcionario-form.component.html',
   styleUrl: './funcionario-form.component.css'
 })
-export class FuncionarioFormComponent {
+export class FuncionarioFormComponent implements OnInit{
+
+  funcionarioForm!: FormGroup;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    this.funcionarioForm = new FormGroup({
+      id: new FormControl(0),
+      nome: new FormControl(""),
+      sobrenome: new FormControl(""),
+      departamento: new FormControl(""),
+      turno: new FormControl(""),
+      ativo: new FormControl(true),
+      DataDeCriacao: new FormControl(new Date()),
+      DataDeAlteracao: new FormControl(new Date())
+    });
+  }
+
+  submit(){
+    console.log(this.funcionarioForm.value);
+  }
 
 }
