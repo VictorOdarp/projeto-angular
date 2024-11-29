cd4af1c922ca1cff870fdbc0d7c6eade450aecde
Victor Eduardo <VictorOdarp@gmail.com>
2024-11-28 23:13:44 -0300

    Desenvolvendo a parte em que aparece as informações do funcionário para exclusão

--- src/app/pages/excluir/excluir.component.ts
+++ src/app/pages/excluir/excluir.component.ts
@@ -1,21 +1,37 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Funcionario } from '../../models/Funcionarios';
 
 // Angular Material //
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule } from '@angular/material/dialog';
-import { Funcionario } from '../../models/Funcionarios';
+import { MatInputModule } from '@angular/material/input';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-excluir',
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule],
+  imports: [MatButtonModule, MatDialogModule, MatInputModule, RouterModule],
   templateUrl: './excluir.component.html',
   styleUrl: './excluir.component.css',
 })
-export class ExcluirComponent {
+export class ExcluirComponent implements OnInit{
 
-  ExcluirFuncionario(){
-    
+  funcionario?: Funcionario;
+  id!:number;
+
+  constructor(private funcionarioService: FuncionarioService, private route: ActivatedRoute, private router: Router){}
+
+  ngOnInit(): void {
+
+    this.id = Number(this.route.snapshot.paramMap.get("id"));
+
+    this.funcionarioService.GetFuncionario(this.id).subscribe((data =>{
+      const dados = data.data;
+
+      this.funcionario = dados;
+    }));
   }
 
 }
